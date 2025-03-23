0e6fa15ffca28696d74ab515fdee4f24764063a7
svetozarov <126297582+svetozarov@users.noreply.github.com>
2025-03-23 10:14:09 +0300

    Update install.py

--- install.py
+++ install.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="AS_LLM_nodes",
-    version="1.0.0",
+    version="1.0.1",
     description="A ComfyUI extension providing Gemini and ChatGPT integration via custom nodes.",
     author="Artem Svetozarov",
     author_email="art.svetozarov@gmail.com",
